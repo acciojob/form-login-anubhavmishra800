550c713210cc6034a4634b3a63684f83d1775f45
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-05-21 13:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const=document.getElementById("fname").value;
+	const=document.getElementById("lname").value;
+
+	alert(fname+" "+lname);
 
 }
